8babfa5c839666234fce2d22dcb5041df23663fa
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-04-05 17:29:59 +0530

    Javascript, JS introduction, JS Basics etc

---
+++ Javascript, JS introduction, JS Basics, JS Variables, JS constants, JS Primitive/Find the value.js
@@ -0,0 +1,17 @@
+/*
+Find the value
+easy
+Problem Statement
+Create an array named arr which contains the first 5 multiples of 2. After this create a variable named val which stores the middle element of the array.
+
+NOTE -You don't have to return anything. You just have to initialize the variables and assign them values
+Input
+There is no input required for this question
+Output
+There is no output required for this question
+*/
+
+// Solution :--
+
+let arr=[2,4,6,8,10];
+let val=6;
